fdda1e6a533d7c3b11f814cbb725fcd51a8eb786
Augustus Otu <Augani7@gmail.com>
2021-05-31 22:08:24 +0000

    switching colors every second

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
+use std::time::SystemTime;
 use sdl2::rect::Rect;
 use std::thread::sleep;
 use sdl2::render::{Canvas,Texture, TextureCreator};
@@ -40,17 +41,10 @@ pub fn main() {
     let window = video_subsystem.window("Tetris", 800, 600).position_centered().opengl().build().expect("Failed to create window");
     let mut canvas = window.into_canvas().build().expect("Failed to create canvas from window");
     let texture_creator: TextureCreator<_> = canvas.texture_creator();
-    let mut square_texture: Texture = texture_creator.create_texture_target(None, TEXTURE_SIZE, TEXTURE_SIZE).expect("Failed to create");
-    canvas.with_texture_canvas(&mut square_texture, |texture| {
-        texture.set_draw_color(Color::RGB(0,255,0));
-        texture.clear();
-    }).expect("Failed to add square");
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
-    canvas.copy(&square_texture, None, Rect::new(0, 0, TEXTURE_SIZE, TEXTURE_SIZE)).expect("Couldn't copy texture to window");
-    canvas.clear();
-    canvas.present();
+    let green_square = create_texture_rect(&mut canvas, &texture_creator, TextureColor::Green,TEXTURE_SIZE).expect("Failed to create green texture");
+    let blue_square = create_texture_rect(&mut canvas, &texture_creator, TextureColor::Blue,TEXTURE_SIZE).expect("Failed to create green texture");
+    let timer =  SystemTime::now();
     let mut event_pump = sdl_context.event_pump().expect("Failed to get sdl event pump");
-
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -63,6 +57,18 @@ pub fn main() {
         }
         canvas.set_draw_color(Color::RGB(255,0,0));
         canvas.clear();
+        let display_green= match timer.elapsed() {
+            Ok(elapsed) => elapsed.as_secs() % 2 == 0,
+            Err(_) => {
+                true
+            }
+        };
+
+        let square_texture = if display_green {
+            &green_square
+        } else {
+            &blue_square
+        };
         canvas.copy(&square_texture, None, Rect::new(0, 0, TEXTURE_SIZE, TEXTURE_SIZE)).expect("Failed to copy square texture");
         canvas.present();
         sleep(Duration::new(0, 1_000_000_000u32 / 60));
